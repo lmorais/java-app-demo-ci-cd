04fc7a955b2e1f698df85057abe2a15e2e2d81f6
Leo Morais <leonel.morais@yahoo.com.br>
2018-10-23 13:29:09 -0300

    Added tests

---
+++ src/test/java/io/rive/demo/HttpRequestTest.java
@@ -0,0 +1,32 @@
+package io.rive.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void helloShouldReturnHelloWorldMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hello/LEONEL",
+                String.class)).contains("Hello  <b>LEONEL</b> , you are welcome!");
+    }
+}
